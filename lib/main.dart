4672889b371a0149b7ca1bff058d45bd08bf9e90
Chanuth412-pixel <e21007@eng.pdn.ac.lk>
2025-05-29 13:24:27 +0530

    Update Flutter + Firebase app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // This is the generated file
+import 'create_farmer_profile_screen.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Agrimate',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: CreateFarmerProfileScreen(),
+    );
+  }
+}
